7b40fa44ba3c76c1baf8c0dcf14a38ed2c536998
MrDant <mael.dantuono@gmail.com>
2020-08-11 01:44:25 +0200

    Add current box view on options

--- src/options/App.vue
+++ src/options/App.vue
@@ -1,20 +1,18 @@
 <template>
   <div style="width:100vw">
     <Header />
-<<<<<<< HEAD
     <div v-if="selectedBox" class="siiimple-card">
       <div class="siimple-card-body siimple--mx-5 siimple--my-2" style="position: relative">
         <div
           class="siimple-btn siimple-btn--orange siimple--mx-2 siimple--my-2"
           style="position:absolute; top:0; right: 0"
-          @click="syncSelectedBoxInfo"
-          :disabled="loading.box"
+          @click="syncSelectedBox"
         >
-          <div class="load load-bouton" v-if="loading.box">
+          <div class="load" v-if="load">
             <i class="fas fa-sync siimple--ml-1"></i>
           </div>
 
-          <b :style="'visibility:' + (loading.box ? 'hidden' :'visible')">Synchroniser</b>
+          <span :style="'visibility:' + (load ? 'hidden' :'visible')">Synchronize</span>
         </div>
 
         <div class="siimple-grid">
@@ -37,144 +35,40 @@
         </div>
       </div>
     </div>
-    <div class="siimple-card siimple--my-3">
-      <div class="siimple-card-header siimple--mx-5">
-        <div class="row">
-          <label class="siimple-label">Les activités:</label>
-          <input
-            class="siimple-select siimple-btn--white"
-            list="browsers"
-            name="browser"
-            id="browser"
-            v-model="activitySelected"
-          />
-          <datalist id="browsers">
-            <option
-              v-for="(activity, index) of selectedBox ? selectedBox.activities_name : []"
-              :key="index"
-            >{{activity}}</option>
-          </datalist>
-          <div
-            class="siimple-btn siimple-btn--orange siimple--mx-2 siimple--my-2"
-            @click="addActivity()"
-          >Ajouter</div>
-
-          <span
-            :class="{load: loading.activities}"
-            class="hover siimple--mx-2"
-            style="position: relative"
-            @click="syncBoxActivities()"
-            :disabled="loading.activities"
-          >
-            <i class="fas fa-sync"></i>
-          </span>
-          <div
-            class="siimple-btn siimple-btn--primary siimple--ml-auto"
-            @click="search()"
-          >Rechercher</div>
-        </div>
-        <div class="siimple-progress siimple-progress--success" v-if="loading.activities">
-          <span :style="{width: `${loading.activities}%`}">{{loading.activities}}% Completed</span>
-        </div>
-        <div class="row">
-          <span
-            class="chips siimple-tag siimple-tag--dark hover siimple--mx-3"
-            v-for="(activity, index) of activitiesFilter.activities"
-            :key="index"
-            @click="removeActivity(activity)"
-          >
-            {{activity}}
-            <i class="far fa-times-circle" style="padding-left: 0.5rem;"></i>
-          </span>
-        </div>
-      </div>
-      <div class="siimple-card-body siimple--mx-5 siimple--text-center activities-container">
-        <span v-if="this.activities.length == 0">Aucun résultat</span>
-        <Activity v-for="(activity, index) of activities" :key="index" :activity="activity" />
-      </div>
-    </div>
-=======
->>>>>>> 2efe523... update structure
   </div>
 </template>
 
 <script>
 import Header from "../common/components/Header";
-<<<<<<< HEAD
-import Activity from "./Activity";
 import BoxService from "../services/boxService";
-import { map, filter } from "rxjs/operators";
-import { BehaviorSubject } from "rxjs";
-=======
->>>>>>> 2efe523... update structure
+import { map } from "rxjs/operators";
 export default {
   name: "App",
   data: () => {
     return {
       idBox: new URLSearchParams(window.location.search).get("box"),
-<<<<<<< HEAD
       selectedBox: null,
       connected: true,
       boxService: null,
-      loading: { box: false, activities: 0 },
-      activitiesProgress: null,
-      activitySelected: "",
+      load: false,
       translation: {
         box_type: "Type",
         price: "Prix",
         quantity: "Quantité",
         variant: "Variante",
       },
-      activitiesFilter: {
-        activities: [],
-      },
-      activities: [],
-=======
-      connected: true,
->>>>>>> 2efe523... update structure
     };
   },
   components: {
     Header,
-<<<<<<< HEAD
-    Activity,
   },
   methods: {
-    removeActivity(activity) {
-      this.activitiesFilter.activities = this.activitiesFilter.activities.filter(
-        (name) => name != activity
-      );
-    },
-    addActivity() {
-      if (
-        !this.activitiesFilter.activities.includes(this.activitySelected) &&
-        this.activitySelected != ""
-      ) {
-        this.activitiesFilter.activities.push(this.activitySelected);
-      }
-      this.activitySelected = "";
-    },
-    syncSelectedBoxInfo() {
-      this.loading.box = true;
-      this.boxService.syncBoxInfo(this.selectedBox).subscribe({
-        complete: () => (this.loading.box = false),
+    syncSelectedBox() {
+      this.load = true;
+      this.boxService.syncBox(this.selectedBox).subscribe({
+        next: (box) => (this.selectedBox = box),
+        complete: () => (this.load = false),
       });
-    },
-    watchSelectedBox() {
-      this.boxService
-        .getBoxById(this.idBox)
-        .pipe(filter((data) => data != null))
-        .subscribe((box) => {
-          this.selectedBox = box;
-        });
-    },
-    syncBoxActivities() {
-      this.activitiesProgress.next(1);
-      this.boxService
-        .syncBoxActivities(this.selectedBox, this.activitiesProgress)
-        .subscribe({
-          complete: () => this.activitiesProgress.next(0),
-        });
     },
     getInfoFiltered(box) {
       if (!box) return null;
@@ -186,24 +80,25 @@ export default {
       });
       return res;
     },
-    search() {
-      this.addActivity();
-      this.activities = this.selectedBox.activities.filter((activity) =>
-        this.activitiesFilter.activities.find((name) => {
-          return !!activity.name.toLowerCase().match(name.toLowerCase());
-        })
-      );
-    },
   },
   created() {
-    this.activitiesProgress = new BehaviorSubject(0);
-    this.activitiesProgress.subscribe(
-      (progress) => (this.loading.activities = progress)
-    );
     this.boxService = BoxService.getInstance();
-    this.watchSelectedBox();
-=======
->>>>>>> 2efe523... update structure
+    this.boxService
+      .getBoxById(this.idBox)
+      .pipe(
+        map((box) => {
+          if (!box) return null;
+          Object.keys(box.info).forEach((label) => {
+            if (!this.translation[label]) {
+              delete box.info[label];
+            }
+          });
+          return box;
+        })
+      )
+      .subscribe((box) => {
+        this.selectedBox = box;
+      });
   },
 };
 </script>
@@ -218,14 +113,7 @@ export default {
   width: 50%;
 }
 
-.chips {
-  font-size: 12px;
-  display: flex;
-  align-items: center;
-  margin: 0.5rem 0;
-}
-
-.load-bouton {
+.load {
   position: absolute;
   top: 0;
   bottom: 0;
@@ -239,29 +127,6 @@ export default {
   -webkit-animation: rotation 2s linear infinite;
   -moz-animation: rotation 2s linear infinite;
   -ms-animation: rotation 2s linear infinite;
-}
-
-.siimple-card-header {
-  display: flex;
-  flex-direction: column;
-}
-
-.siimple-progress {
-  background-color: white;
-}
-
-.activities-container {
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-  justify-content: space-around;
-}
-
-.row {
-  display: flex;
-  align-items: center;
-  justify-content: start;
-  flex-wrap: wrap;
 }
 
 @-webkit-keyframes rotation {
--- src/services/boxService.js
+++ src/services/boxService.js
@@ -1,18 +1,10 @@
 import { map, catchError, tap, switchMap } from "rxjs/operators";
 import Storage from "../common/utils/storage";
 import Box from "../models/box";
-<<<<<<< HEAD
-import Activity from "../models/activity";
-import StreamReader from "../common/utils/streamReader";
-import Api from "../models/api";
-import ChromeWindow from "../common/utils/chromeWindow";
-import { NEVER, throwError, BehaviorSubject, EMPTY } from "rxjs";
-=======
 import StreamReader from "../common/utils/streamReader";
 import Api from "../models/api";
 import ChromeWindow from "../common/utils/chromeWindow";
 import { NEVER, throwError, BehaviorSubject } from "rxjs";
->>>>>>> 2efe523... update structure
 
 export default class BoxService {
   constructor() {
@@ -44,13 +36,6 @@ export default class BoxService {
     Storage.set({ boxes: val });
   }
 
-<<<<<<< HEAD
-  saveBox(box) {
-    let boxes = this.boxesSubject.getValue();
-    Object.assign(boxes[box.info.id], box);
-    this.boxes$ = boxes;
-  }
-
   getBoxById(id) {
     return this.boxesSubject.pipe(
       map((boxes) => {
@@ -59,66 +44,6 @@ export default class BoxService {
     );
   }
 
-  syncBoxActivities(box, progress) {
-    let stream = new StreamReader(
-      `${Api.activities}?pim_id=${box.id}&pagesize=200&page=1&sortby=position`
-    );
-    return stream.extractDataToJson().pipe(
-      catchError((error) => {
-        console.log(error);
-        if (error.status == 301 && error.url == Api.login) {
-          ChromeWindow.focusTo(Api.login);
-        }
-        return EMPTY;
-      }),
-      switchMap(async (data) => {
-        progress.next(this.progressNumber(data));
-        box.total = data.total;
-        box.activities = data.items.map((raw) => new Activity(raw));
-        while (data.items.length == 200) {
-          let stream = new StreamReader(
-            `${Api.activities}?pim_id=${box.id}&pagesize=200&page=${data.page +
-              1}`
-          );
-          data = await stream.extractDataToJson().toPromise();
-          box.activities = box.activities.concat(
-            data.items.map((raw) => new Activity(raw))
-          );
-
-          progress.next(this.progressNumber(data));
-        }
-        box.activities_name = box.activities
-          .map((activity) => activity.name)
-          .reduce((acc, current) => {
-            if (current != "" && !acc.includes(current)) acc.push(current);
-            return acc;
-          }, [])
-          .sort();
-        this.saveBox(box);
-      })
-    );
-  }
-
-  syncBoxInfo(box) {
-    return new StreamReader(box.link).extractData().pipe(
-      catchError((error) => {
-        if (error.status == 301) {
-          return new StreamReader(error.url).extractData();
-        }
-        return throwError(error);
-      }),
-      map((str) => {
-        const match = str.match(/dataLayer.*\}\)\;/g)[0];
-        const obj = JSON.parse(match.substring(15, match.length - 2));
-        if (obj.ecommerce) {
-          ChromeWindow.focusTo(Api.login);
-          return EMPTY;
-        }
-        box.info = obj.redemption_products[0];
-        box.name = box.info.name;
-        box.category = box.info.category;
-        this.saveBox(box);
-=======
   getBoxActivities(box) {
     let stream = new StreamReader(
       `${Api.activities}?pim_id=${box.id}&pagesize=200`
@@ -131,7 +56,30 @@ export default class BoxService {
       }),
       map((data) => {
         return data.items;
->>>>>>> 2efe523... update structure
+      })
+    );
+  }
+
+  syncBox(box) {
+    return new StreamReader(box.link).extractData().pipe(
+      catchError((error) => {
+        if (error.status == 301) {
+          return new StreamReader(error.url).extractData();
+        }
+        return throwError(error);
+      }),
+      map((str) => {
+        console.log(box.url);
+        const match = str.match(/dataLayer.*\}\)\;/g)[0];
+        const obj = JSON.parse(match.substring(15, match.length - 2));
+        console.log(obj);
+        box.info = obj.redemption_products[0];
+        box.name = box.info.name;
+        box.category = box.info.category;
+        let boxes = this.boxesSubject.getValue();
+        boxes[box.info.id] = box;
+        this.boxes$ = boxes;
+        return box;
       })
     );
   }
@@ -154,17 +102,10 @@ export default class BoxService {
           return Array.from(elements).map((element) => new Box(element));
         }),
         switchMap(async (boxes) => {
-<<<<<<< HEAD
           const result = {};
           for (const box of boxes) {
             let updateBox = await box.getInfo().toPromise();
             result[updateBox.id] = updateBox;
-=======
-          const result = [];
-          for (const box of boxes) {
-            console.log(box);
-            result.push(await box.getInfo().toPromise());
->>>>>>> 2efe523... update structure
           }
           return result;
         }),
@@ -173,17 +114,4 @@ export default class BoxService {
         })
       );
   }
-<<<<<<< HEAD
-
-  progressNumber(data) {
-    return Math.round(
-      (data.page /
-        (data.total % data.pagesize > 0
-          ? Math.round(data.total / data.pagesize) + 1
-          : data.total / data.pagesize)) *
-        100
-    );
-  }
-=======
->>>>>>> 2efe523... update structure
 }
