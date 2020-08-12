b5c0f369400d05aa1609e22ec8bad25a0fc560e1
MrDant <mael.dantuono@gmail.com>
2020-08-12 03:43:32 +0200

    create Activity component

---
+++ src/options/Activity.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="siimple-card" @click="goToBuy()" style="width: max-content">
+    <div class="siimple-card-body">
+      <img :src="activity.image_gallery[0]" />
+      <div>{{activity.name}}</div>
+    </div>
+  </div>
+</template>
+<script>
+import ChromeWindow from "../common/utils/chromeWindow";
+export default {
+  name: "App",
+  props: ["activity"],
+  data: () => {
+    return {};
+  },
+  components: {},
+  methods: {
+    goToBuy() {
+      ChromeWindow.focusTo(this.activity.url);
+    },
+  },
+  created() {
+    console.log(this.activity);
+  },
+};
+</script>
+
+<style scoped>
+</style>
