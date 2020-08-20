de173a8ff22b7cd5f6602c4edf897820ae057876
MrDant <mael.dantuono@gmail.com>
2020-08-20 03:52:48 +0200

    refactor location

--- src/options/Activity.vue
+++ src/options/Activity.vue
@@ -2,16 +2,18 @@
   <div class="siimple-card">
     <div class="siimple-card-body">
       <Carousel :sliderClass="sliderClass">
-        <div class="slide" v-for="(img, index) of activity.image_gallery" :key="index">
+        <div class="slide pointer" v-for="(img, index) of activity.image_gallery" :key="index">
           <img :src="img" @click="goToBuy()" />
         </div>
       </Carousel>
       <div class="activity-name">{{ activity.name }}</div>
       <div class="activity-details primary">
         <div id="description">{{ activity.short_description }}</div>
-        <div id="distance" v-if="distance">
-          <i class="fas fa-map-marker-alt"></i>
-          {{ distance }} km
+        <div id="distance" v-if="!!this.distance.maps">
+          <a :href="this.distance.maps" class="hover primary" target="_blank">
+            <i class="fas fa-map-marker-alt"></i>
+            {{ this.distance.km }} km
+          </a>
         </div>
       </div>
     </div>
@@ -19,14 +21,16 @@
 </template>
 <script>
 import ChromeWindow from "../common/utils/chromeWindow";
+import StreamReader from "../common/utils/streamReader";
 import Carousel from "../common/components/Carousel";
+import Api from "../models/api";
 export default {
   name: "App",
   props: ["activity"],
   data: () => {
     return {
       sliderClass: "hover",
-      distance: null,
+      distance: { maps: null, km: null },
     };
   },
   components: {
@@ -36,24 +40,38 @@ export default {
     goToBuy() {
       ChromeWindow.focusTo(this.activity.url);
     },
+    getMapLink() {
+      let getMinute = (val) => {
+        return +("0." + val.toString().split(".")[1]) * 60;
+      };
+      let formatCoordinate = (val) => {
+        return {
+          deg: val.toString().split(".")[0],
+          min: Math.floor(getMinute(val)),
+          sec: Math.floor(getMinute(getMinute(val)) * 10) / 10,
+        };
+      };
+      let lat = formatCoordinate(this.activity.gps_latitude);
+      let long = formatCoordinate(this.activity.gps_longitude);
+      return `${Api.googleMaps}${Math.abs(lat.deg)}°${lat.min}'${lat.sec}${
+        lat.deg < 0 ? "S" : "N"
+      }+${Math.abs(long.deg)}°${long.min}'${long.sec}${
+        long.deg < 0 ? "W" : "E"
+      }`;
+    },
     getDistance(lat1, lat2, lon1, lon2) {
-      var R = 6378.137; // Radius of earth in KM
-      var dLat = (lat2 * Math.PI) / 180 - (lat1 * Math.PI) / 180;
-      var dLon = (lon2 * Math.PI) / 180 - (lon1 * Math.PI) / 180;
-      var a =
-        Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-        Math.cos((lat1 * Math.PI) / 180) *
-          Math.cos((lat2 * Math.PI) / 180) *
-          Math.sin(dLon / 2) *
-          Math.sin(dLon / 2);
-      var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-      var d = R * c;
-      return Math.round(d * 100) / 100;
+      let stream = new StreamReader(
+        `http://router.project-osrm.org/route/v1/car/${lon1},${lat1};${lon2},${lat2}`
+      );
+      stream.extractDataToJson().subscribe((data) => {
+        this.distance.km = Math.floor(data.routes[0].distance / 10) / 100;
+      });
+      this.distance.maps = this.getMapLink();
     },
   },
   created() {
     navigator.geolocation.getCurrentPosition((geo) => {
-      this.distance = this.getDistance(
+      this.getDistance(
         geo.coords.latitude,
         this.activity.gps_latitude,
         geo.coords.longitude,
